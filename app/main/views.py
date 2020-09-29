12ac11bfc47df80ae4162a8280a2548e5e70fb23
kilewa <kilewageorge230@gmail.com>
2020-09-29 18:43:29 +0300

    added cards to dashboard

--- app/main/views.py
+++ app/main/views.py
@@ -11,3 +11,11 @@ def index():
     
     return render_template('index.html')
 
+
+@main.route('/dashboard')
+def dashboard():
+	return render_template('dashboard.html')
+
+
+
+
--- app/merchant/views.py
+++ app/merchant/views.py
@@ -1,12 +1,1 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-	return render_template('index.html')
-
-@app.route('/dashboard')
-def dashboard():
-	return render_template('dashboard.html')
 
