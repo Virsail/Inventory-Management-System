b2c32eaab5059964a7400b72543972cd742b6349
Dennis Kamunya <dennismuriithik@gmail.com>
2020-09-30 11:47:52 +0300

    Create auth login form

---
+++ app/auth/forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,PasswordField,BooleanField,SubmitField,ValidationError 
+from wtforms.validators import Required,Email,EqualTo
+from ..models import User
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Your Email Address',validators=[Required(),Email()])
+    password = PasswordField('Password',validators =[Required()])
+    remember = BooleanField('Remember me')
+    submit = SubmitField('Sign In')          
+
