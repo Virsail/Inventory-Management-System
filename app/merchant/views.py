e85bd9230a5b357fefeeb8acea55d80c47d665fb
Dennis Kamunya <dennismuriithik@gmail.com>
2020-10-01 09:19:32 +0300

    Update migrations

--- app/merchant/views.py
+++ app/merchant/views.py
@@ -1,37 +1,46 @@
 from . import merchant
 from flask import render_template, url_for, flash, redirect
+from flask_login import login_required,current_user
 from .forms import MerchantRegistrationForm
 
 @merchant.route('/')
+@login_required
 def index():
+  print(current_user)
   return render_template('merchant/dashboard.html')
 
 @merchant.route('/clerks')
+@login_required
 def clerks():
   return render_template('merchant/merchant_home.html')
 
 
 
 @merchant.route('/administrators/administrator')
+@login_required
 def clerk_name():
   return render_template('merchant/admin_merchant.html')
 
 
 @merchant.route('/stores')
+@login_required
 def stores():
   return render_template('merchant/stores.html')
 
 
 @merchant.route('/store/reports/<store_name>')
+@login_required
 def store_reports(store_name):
   return render_template('merchant/store_reports.html')
 
 @merchant.route('/product_requisition')
+@login_required
 def product_requisition():
 	return render_template('merchant/product_requisition.html')	
 
 
 @merchant.route('/order_payment_status')
+@login_required
 def order_payment_status():
 	return render_template('merchant/order_payment_status.html')	
 
